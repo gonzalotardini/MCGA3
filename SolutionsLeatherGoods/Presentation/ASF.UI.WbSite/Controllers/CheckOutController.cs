3c40fb73204b91ca43f844ad3f320341c07999a5
gonzalotardini <gonzalotardini@gmail.com>
2017-11-24 14:24:59 -0300

    Se agrego rol admin y la administraciond e la pagina solo se le muestra al usuaario admin. seo da B-

---
+++ SolutionsLeatherGoods/Presentation/ASF.UI.WbSite/Controllers/CheckOutController.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace ASF.UI.WbSite.Controllers
+{
+    public class CheckOutController : Controller
+    {
+        // GET: CheckOut
+        public ActionResult Index()
+        {
+
+            return View();
+        }
+
+        public ActionResult CheckOut()
+        {
+            return View();
+        }
+    }
+}
