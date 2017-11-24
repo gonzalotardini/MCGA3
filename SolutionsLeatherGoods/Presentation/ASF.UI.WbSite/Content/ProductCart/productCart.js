abd0b9436fb593939ea686c8505b6f8d1ee2f0bf
gonzalotardini <gonzalotardini@gmail.com>
2017-11-24 16:15:14 -0300

    Se agreggo qjery de efecto cart

---
+++ SolutionsLeatherGoods/Presentation/ASF.UI.WbSite/Content/ProductCart/productCart.js
@@ -0,0 +1,18 @@
+﻿$(document).ready(function () {
+    $("#carticon").click(function () {
+        $("#cartContent").slideToggle("2000");
+    });
+
+    $("#btn").click(function () {
+
+        var title = $("#title")[0].textContent;
+
+        $("#lista").append(title);
+      
+        
+        
+    });
+
+
+    
+});
